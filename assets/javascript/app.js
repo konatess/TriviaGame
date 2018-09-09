2870b7850d95c4717750c65a01f3fb45b1e70083
Kona Tess <konatess@gmail.com>
2018-09-09 16:28:36 -0700

    Page is prettified and JS is pseudo coded.

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+// array of objects for questions
+    // each question needs 
+        // question
+        // four answers
+        // marker for correct answer
+
+// variables
+
+// functions
+
+// plan:
+    // click start button to begin
+    // play begins
+        // question displays
+        // answers display
+        // clock begins countdown
+    // on click, answers
+        // clock stops
+        // determines clicked is correct or not
+            // if correct, diplays yes
+            // if not, displays no and correct answer
+        // after 5 seconds, goes to next question
+
+// stretch goals
+    // easy, medium and hard option, with different times for each
